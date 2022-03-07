f4faa2a67d136e19c1d0970c93291236471afc0e
Param Raghavan <paramraghavan@gmail.com>
2022-03-07 08:33:51 -0500

    - cleanup

--- sqlDB/sql-step-by-step.sql
+++ sqlDB/sql-step-by-step.sql
@@ -14,41 +14,46 @@ contains 3 tables:
 --    exploring the data, and getting acquainted with the 3 tables. Open db browser
 --    lite, File --> Open Database -->sql\sqlDB\sqlite_db_pythonsqlite.db
 
-
-/* QUESTIONS 
-/* Q1: Some of the facilities charge a fee to members, but some do not.
-Write a SQL query to produce a list of the names of the facilities that do. */
+--    Q1
+--    Some of the facilities charge a fee to members, but some do not.
+--    Write a SQL query to produce a list of the names of the facilities that do.
 
 select name from Facilities where membercost <> 0;
 
-/* Q2: How many facilities do not charge a fee to members? */
+--
+--    Q2: How many facilities do not charge a fee to members?
+--
 
 select count(*) from Facilities where membercost = 0
 
-/* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
-where the fee is less than 20% of the facility's monthly maintenance cost.
-Return the facid, facility name, member cost, and monthly maintenance of the
-facilities in question. */
-
+--
+--    Q3:
+--    Write an SQL query to show a list of facilities that charge a fee to members,
+--    where the fee is less than 20% of the facility's monthly maintenance cost.
+--    Return the facid, facility name, member cost, and monthly maintenance of the
+--    facilities in question.
+--
 SELECT facid, name, membercost, monthlymaintenance, 
 monthlymaintenance*0.2 percentratio
 FROM Facilities
 WHERE membercost < (monthlymaintenance * 0.2)  
 AND membercost <> 0
  
-
-/* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
-Try writing the query without using the OR operator. */
-
+--
+--    Q4:
+--    Write an SQL query to retrieve the details of facilities with ID 1 and 5.
+--    Try writing the query without using the OR operator.
+--
 SELECT * 
 FROM Facilities
 where facid in (1,5)
 
-/* Q5: Produce a list of facilities, with each labelled as
-'cheap' or 'expensive', depending on if their monthly maintenance cost is
-more than $100. Return the name and monthly maintenance of the facilities
-in question. */
-
+--
+--    Q5:
+--    Produce a list of facilities, with each labelled as
+--    'cheap' or 'expensive', depending on if their monthly maintenance cost is
+--    more than $100. Return the name and monthly maintenance of the facilities in question.
+--
 SELECT name,
 CASE WHEN monthlymaintenance >100
 THEN 'Expensive'
@@ -56,9 +61,11 @@ ELSE 'Cheap'
 END
 FROM Facilities
 
-/* Q6: You'd like to get the first and last name of the last member(s)
-who signed up. Try not to use the LIMIT clause for your solution. */
-
+--
+--    Q6:
+--    You'd like to get the first and last name of the last member(s)
+--    who signed up. Try not to use the LIMIT clause for your solution.
+--
 SELECT firstname, surname
 FROM Members
 WHERE joindate = (
@@ -71,11 +78,13 @@ SELECT firstname, surname
 FROM Members
 order by joindate desc limit 1
 
-/* Q7: Produce a list of all members who have used a tennis court.
-Include in your output the name of the court, and the name of the member
-formatted as a single column. Ensure no duplicate data, and order by
-the member name. */
-
+--
+--    Q7:
+--    Produce a list of all members who have used a tennis court.
+--    Include in your output the name of the court, and the name of the member
+--    formatted as a single column. Ensure no duplicate data, and order by
+--    the member name.
+--
 SELECT DISTINCT f.name courtname, CONCAT( firstname, ' ', surname ) membername
 FROM Bookings b
 LEFT JOIN Members m ON b.memid = m.memid
@@ -83,14 +92,15 @@ LEFT JOIN Facilities f ON b.facid = f.facid
 WHERE f.name LIKE 'Tennis Court%'
 ORDER BY membername
 
-
-/* Q8: Produce a list of bookings on the day of 2012-09-14 which
-will cost the member (or guest) more than $30. Remember that guests have
-different costs to members (the listed costs are per half-hour 'slot'), and
-the guest user's ID is always 0. Include in your output the name of the
-facility, the name of the member formatted as a single column, and the cost.
-Order by descending cost, and do not use any subqueries. */
-
+--
+--    Q8:
+--     Produce a list of bookings on the day of 2012-09-14 which
+--    will cost the member (or guest) more than $30. Remember that guests have
+--    different costs to members (the listed costs are per half-hour 'slot'), and
+--    the guest user's ID is always 0. Include in your output the name of the
+--    facility, the name of the member formatted as a single column, and the cost.
+--    Order by descending cost, and do not use any subqueries.
+--
 --SELECT f.name facilityname, CONCAT( firstname, ' ', surname ) membername,
 SELECT f.name facilityname, firstname || ' ' || surname as  membername,
 CASE WHEN m.firstname = 'GUEST'
@@ -108,8 +118,10 @@ ELSE membercost * slots >30
 END
 ORDER BY cost DESC
 
-/* Q9: This time, produce the same result as in Q8, but using a subquery. */
-
+--
+--    Q9:
+--    This time, produce the same result as in Q8, but using a subquery.
+--
 SELECT facilityname, CONCAT( firstname, ' ', surname ) membername, cost
 FROM (
 SELECT firstname, surname,
@@ -125,12 +137,22 @@ WHERE DATE_FORMAT( starttime, '%Y-%m-%d' ) = '2012-09-14'
 WHERE cost >30
 ORDER BY cost DESC
 
-/* PART 2: SQLite
+--
+--    Q10:
+--     Produce a list of facilities with a total revenue less than 1000.
+--    The output of facility name and total revenue, sorted by revenue. Remember
+--    that there's a different 	cost for guests and members!
+--
 
-QUESTIONS:
-/* Q10: Produce a list of facilities with a total revenue less than 1000.
-The output of facility name and total revenue, sorted by revenue. Remember
-that there's a different 	cost for guests and members! */
+-- NOTE:
+-- The WITH clause is considered “temporary” because the result is not permanently stored anywhere
+-- in the database schema. It acts as a temporary view that only exists for the duration of the query.
+-- This veiw is available i the scope of the connection/transaction session scope, on the transaction is commited
+-- or the connection is closed the session goes away. If you have some kind of query rerun again and again to
+-- compute some kind of aggregation which is used by the subsequent queries, it is best toi create this table using
+-- the "WITH clause", this way a view for this query is created and will be reused without
+-- the query getting reexecuted - think this view is cached for the txn/connection scope
+
 
 WITH fac_details AS (select f.facid, f.name facilityname, 
 CASE WHEN m.firstname = 'GUEST'
@@ -167,20 +189,23 @@ group by facility_name having totalrevenue >1000
 --select f.name, f.facid, f.membercost, f.guestcost from Facilities f;
 --Select m.memid, m.firstname from Members m4
 
-/* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
-
+--
+--    Q11:
+--    Produce a report of members and who recommended them in alphabetic surname,firstname order
+--
 select m1.surname || ' ' || m1.firstname membername, 
 m2.surname ||' ' || m2.firstname recommendedBy
 from members m1
 inner join members m2 on m1.recommendedby = m2.memid
 order by m1.surname, m1.firstname
 
-/* Q12: Find the facilities with their usage by member, but not guests */
+--
+--    Q12:
+--    Find the facilities with their usage by member, but not guests
+--    how many times it got booked -like sum of slots
+--
 
-
-how many times it got booked -like sum of slots
-
-    select name, usage 
+   select name, usage
     from Facilities f
     inner join (SELECT facid, sum(slots) usage
         FROM Bookings b, Members m
@@ -189,13 +214,12 @@ how many times it got booked -like sum of slots
         group by facid) a
       where f.facid = a.facid
 
-
-/* Q13: Find the facilities usage by month, but not guests */
-
-facid, month , slots total
-/* EXTRACT and date_format did not work in jupyternotebook */
-
-select name, month, usage 
+--
+--    Q13:
+--    Find the facilities usage by month, but not guests facid, month , slots total
+--    EXTRACT and date_format
+--
+  select name, month, usage
     from Facilities f
     inner join (SELECT facid, strftime("%m", starttime) month, sum(slots) usage
         FROM Bookings b left join Members m
@@ -223,17 +247,20 @@ where totalrevenue < 1000
 
 --
 -- Windows Function
+-- Observe unlike regular aggregate functions, use of a window function does not cause rows to become
+-- "grouped into a single output row" — the rows retain their separate identities.
 --
-
--- Consider a query where we wanted to find the average weight of the runners grouped by country. This can be done like this:
-
+-- Added the fourth table "Runners" table.
+-- Consider a query where we wanted to find the average weight of the runners grouped
+-- by country. This can be done like this:
+--
 select
 country,
 avg(weight)
 from runners group by country;
 
 
-
+-- observe "over ()" in the following, starting to use Windows Function
 -- when over is empty, average weight for all the rows is computed
 select
 name, weight,
